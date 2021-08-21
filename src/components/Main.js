4ea233ff74c5278d374d4e74bd739e7868383b53
Thiago Esquivel <thiago.sesquivel@gmail.com>
2021-08-21 04:03:31 -0300

    Add class responsive

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Main({children}) {
     return (
-        <main className="w-full flex items-center justify-center lg:p-10 p-2" style={{height: `calc( 100vh - 4rem )`}}>
+        <main className="w-full flex items-center justify-center lg:p-10 p-2" >
             {children}
         </main>
     )
