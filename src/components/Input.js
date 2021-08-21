9cdb039bdb7d909a2e02f71907431510291924a2
Thiago Esquivel <thiago.sesquivel@gmail.com>
2021-08-21 04:05:10 -0300

    Edit props

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,7 +1,7 @@
 import React,{useEffect, useRef} from 'react'
 import { useField} from '@unform/core'
 
-function Input({name, label, ...rest}) {
+function Input({name, label, className, ...rest}) {
     const inputRef =  useRef(null);
     const { fieldName, registerField, error} = useField(name)
 
@@ -22,9 +22,9 @@ function Input({name, label, ...rest}) {
     },[fieldName, registerField]);
 
     return (
-        <div className="w-full flex flex-col mb-2">
+        <div className={`w-full flex flex-col mb-2`}>
             <label className="font-bold text-base mb-1">{label}</label>
-            <input ref={inputRef} className={` focus:border-blue-500 p-2 border-solid border focus:outline-none block w-full focus:ring focus:ring-blue-500 focus:ring-opacity-50 rounded-md shadow-sm ${error?'border-red-400':'border-gray-300' }` } name={name} {...rest} />
+            <input ref={inputRef} className={` focus:border-blue-500 p-2 border-solid border focus:outline-none block w-full focus:ring focus:ring-blue-500 focus:ring-opacity-50 rounded-md shadow-sm ${className && className} ${error?'border-red-400':'border-gray-300' }` } name={name} {...rest} />
             {error && <span className="mt-1 text-red-500 w-full">{error}</span>}
         </div>
     )
