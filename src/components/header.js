a6753e6a107018b195f01dbb8147ac474d1d49be
Thiago Esquivel <thiago.sesquivel@gmail.com>
2021-08-21 04:07:01 -0300

    Add logout button

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import Logo from '../assets/logo-blog.png'
 
-function Header({dropdown = null}) {
+function Header({logged}) {
     return (
-        <header className="w-full h-16 bg-blue-500 p-2 pl-4 pr-4">
-            <img src={Logo} alt="logo" className="h-full w-auto"></img>
-            { dropdown && dropdown}
+        <header className="w-full h-16 bg-blue-500 flex justify-between pr-4 pl-4">
+            <div className="h-full p-2">
+                <img src={Logo} alt="logo" className="h-full w-auto"></img>
+            </div>
+            { logged && <Link className="mt-2 mb-4 rounded shadow-md hover:bg-opacity-80 p-2 pl-6 pr-6 bg-red-500 text-white font-bold">Sair</Link>}
         </header>
     )
 }
